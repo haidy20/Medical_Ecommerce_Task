90dd165a1356d5028ff06e9f2469010700d22eb4
haidy esaam <dodyessam667@gmail.com>
2025-05-24 18:26:36 +0300

    some details

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // $this->call(AdminUserSeeder::class);
+        $this->call(AdminUserSeeder::class);
 
         // ننشئ 10 تصنيفات (Categories)
         \App\Models\Category::factory(10)->create();
